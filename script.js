9d886803f4e3fa66c76a2949c18f0d0c49bfc74c
Kateryna-123 <knovizkaa@gmail.com>
2021-03-09 20:45:00 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+//alert('gfjfke')
+
+const name = 'kateryna';
+let age = 24;
+let car = true;
+let money = false;
+
+
+age = 45;
+
+//document.write('hello my name is ' + name + ' and i am ' + age + ' years old');
+
+document.write(2 + 2)
+document.write('<hr>')
+document.write(2 - 2)
+document.write('<hr>')
+document.write(2 * 2)
+document.write('<hr>')
+document.write(2 / 2)
+document.write('<hr>')
+document.write((2 + 2) * 2)
+document.write('<hr>')
+document.write(10 % 3)   //остача от деления
+
+
+console.log(true + 2);   //=3 true консоль воспринимает как 1
+console.log(false + 5);
+console.log(10 + '1');     //значение в скобке не изменно и не плюсуется
+console.log(10 - '1');     //значение в скобке не изменно, при отнимании производит функцию отнимания числа от числа
+console.log(10 - 'hello');     //значение в скобке не изменно, при отнимании числа от слова выбивает ошибку
+
+
+
